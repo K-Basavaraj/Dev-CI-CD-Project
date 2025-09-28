9c076860325aa3a0aa64be2402587e76c297f1e4
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2025-09-28 15:01:14 +0530

    configured jenkins pipelines

--- infra-cicd/50-eks/providers.tf
+++ infra-cicd/50-eks/providers.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.85.0"
+      version = "5.95.0"
     }
   }
   backend "s3" {
     bucket         = "infracicd-dev-s3"
-    key            = "sg-dev"
+    key            = "eks-dev"
     region         = "us-east-1"
     dynamodb_table = "infra-dev-locking"
   }
--- infra-cicd/70-ecr/providers.tf
+++ infra-cicd/70-ecr/providers.tf
@@ -2,17 +2,19 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.85.0"
+      version = "5.66.0"
     }
   }
+
   backend "s3" {
     bucket         = "infracicd-dev-s3"
-    key            = "sg-dev"
+    key            = "k8-ecr-dev"
     region         = "us-east-1"
     dynamodb_table = "infra-dev-locking"
   }
 }
 
 provider "aws" {
+  # Configuration options
   region = "us-east-1"
 }
